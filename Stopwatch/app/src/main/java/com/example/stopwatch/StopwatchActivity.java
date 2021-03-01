d8161225729aceaef21c28e8eb607df19f15e52c
Nirlley Costa <nirlleycosta@gmail.com>
2021-02-28 22:47:54 -0300

    Commit adicionando arquivos de Classe e Layout

---
+++ Stopwatch/app/src/main/java/com/example/stopwatch/StopwatchActivity.java
@@ -0,0 +1,17 @@
+package com.example.stopwatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class StopwatchActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stopwatch);
+    }
+}
